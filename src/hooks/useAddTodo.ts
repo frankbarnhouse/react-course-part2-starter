b951cde9f64d6b0ca03e5a9a04a4fb32b8be535d
Frank Barnhouse <frank@thinkanewmedia.com>
2023-08-25 14:18:31 +0100

    refactored to use todoService

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -1,10 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Todo } from "./useTodos";
-import axios from "axios";
-import APIClient from "../react-query/services/apiClient";
 import { CACHE_KEY_TODOS } from "../react-query/constants";
-
-const apiClient = new APIClient<Todo>('/todos')
+import todoService, { Todo } from "../react-query/services/todoService";
 
 interface AddTodoContext {
     previousTodos: Todo[];
@@ -13,7 +9,7 @@ interface AddTodoContext {
 const useAddTodo =(onAdd: () => void) => {
     const queryClient = useQueryClient();
     return useMutation<Todo, Error, Todo, AddTodoContext>({
-      mutationFn: apiClient.post,
+      mutationFn: todoService.post,
       onMutate: (newTodo: Todo) => {
         const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
   
