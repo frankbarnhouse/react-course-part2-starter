3ba9faed025bdd7bef50631ca577e447045436d7
Frank Barnhouse <frank@thinkanewmedia.com>
2023-09-04 17:26:10 +0100

    Saving working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import "./App.css";
-import PostList from "./react-query/PostList";
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
+import Counter from "./state-management/counter/Counter";
+import HomePage from "./state-management/HomePage";
+import NavBar from "./state-management/NavBar";
+import { TasksProvider } from "./state-management/tasks";
 
 function App() {
   return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
+    <TasksProvider>
+      <Counter />
+      <NavBar />
+      <HomePage />
+    </TasksProvider>
   );
-
-  // return <PostList />;
 }
 
 export default App;
